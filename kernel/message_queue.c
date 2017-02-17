03aca5a07ee0f17cae8086af4fb205cade2c0e71
Marie-Alix Juffard <marie-alix.juffard@ensimag.grenoble-inp.fr>
2017-02-17 09:31:19 +0100

    message_queue count et debut pdelete

--- kernel/message_queue.c
+++ kernel/message_queue.c
@@ -8,12 +8,22 @@
 int nb_queue = 0;
 struct queue **queues = NULL;// /*malloc(/*NBQUEUE **/ 1/*sizeof(*queues)*/)*/;
 
-/*int pcount(int fid, int *count){
-  if (count != NULL) {
-
+int pcount(int fid, int *count){
+    int i = 0;
+  while (i < nb_queue && queues[i]->fid != fid) {
+    i++;
   }
-  return -1;
-  }*/
+  if (i == nb_queue) {
+    return -1;
+  }
+  if (count != NULL) {
+    if (queues[i]->nb_message == 0) {
+      return - queues[i]->nb_p_bloques;
+    }
+    return queues[i]->nb_message + queues[i]->nb_p_bloques;
+  }
+  return 0;
+ }
 
 int pcreate(int count){
   if (queues == NULL) {
@@ -25,6 +35,8 @@ int pcreate(int count){
     queues[nb_queue]->nb_message = 0;
     queues[nb_queue]->size_max = count;
     queues[nb_queue]->messages = malloc (count * sizeof(int));
+    queues[nb_queue]->nb_p_bloques = 0;
+    queues[nb_queue]->p_bloques = malloc (PROCESS_TABLE_SIZE * sizeof(int));
     return nb_queue - 1;
   } else {
     return -1;
@@ -40,7 +52,15 @@ int pdelete(int fid){
   if (i == nb_queue) {
     return -1;
   }
+  for (int j = 0; j < queues[i]->nb_p_bloques; j++) {
+
+  }
+  queues[i]->nb_p_bloques = 0;
+  //free(queues[i]->p_bloques);
+  queues[i]->nb_message = 0;
+  queues[i]->size_max = 0;
   //free(queues[i]->messages);
+
 
 
 
