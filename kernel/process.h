a8505e88e36c922d3a8ac6711aa4568b3d682efa
Duc-Anh Luu <Duc-Anh.Luu@grenoble-inp.org>
2017-03-07 14:44:38 +0100

    Finish the implementation of semaphore
    Passed tests (part semaphore): 10, 15

--- kernel/process.h
+++ kernel/process.h
@@ -18,13 +18,18 @@ void maj_sleeping(int pid);
 int mon_pid();
 void dors(int nbr_secs);
 
+// SEMAPHORE
+void bloque_sur_semaphore();
+void passe_activable(int pid);
+
+
 struct process* process_list_head;
 struct process* process_list_tail;
 
 struct process* sleeping_list_head;
 struct process* sleeping_list_tail;
 
-typedef enum {CHOSEN, ACTIVABLE, SLEEPING} process_state;
+typedef enum {CHOSEN, ACTIVABLE, SLEEPING, BLOCKED_ON_SEM} process_state;
 
 struct process {
 	int pid;
--- kernel/sem.h
+++ kernel/sem.h
@@ -2,7 +2,7 @@
 #define _SEM_H_
 #include "mem.h"
 
-#define NBSEM 5
+#define NBSEMS 5
 
 int scount(int sem);
 int screate(short count);
@@ -13,9 +13,15 @@ int sreset(int sem, short count);
 int try_wait(int sem);
 int wait(int sem);
 
+struct procs_bloques {
+    int pid;
+    struct procs_bloques *next;
+};
+
 struct sem {
     int sid;
     short val;
     struct sem *next;
+    struct procs_bloques *p_bloques; // liste des processus bloqués sur sémaphore
 };
 #endif
