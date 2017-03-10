98537db7dbeb1346cbba32a02e1feadf8b79c3bf
Guillaume Gremillet (Guigre) <gui.gremi@gmail.com>
2017-03-10 10:39:10 +0100

    CLean kernel start code, random testing code was here...

--- kernel/start.c
+++ kernel/start.c
@@ -17,12 +17,12 @@ int fact(int n)
 
 void kernel_start(void)
 {
-	int i;
-//	call_debugger();
+        cli();
+	set_clock_fqc();
+	init_traitant_IT(32, &traitant_IT_32);
+	masque_IRQ(0,0);
 
-	i = 10;
-
-	i = fact(i);
+	start("autotest", 4000, 128, NULL);
 
 	cli();
 	set_clock_fqc();
@@ -32,44 +32,6 @@ void kernel_start(void)
 	printf("\f");
 
 	printf("group_14@awsome_pc:~$\n");
-/* ------------------------------------------------ */
-/* ---- TEST 10 - SEMAPHORE ----------------------- */
-/* ------------------------------------------------ */
-        int sem;
-        assert(screate(-2) == -1);
-        assert((sem = screate(2)) >= 0);
-        assert(signaln(sem, -4) < 0);
-        assert(sreset(sem, -3) == -1);
-        assert(scount(sem) == 2);
-        assert(signaln(sem, 32760) == 0);
-        assert(signaln(sem, 6) == -2);
-        assert(scount(sem) == 32762);
-        assert(wait(sem) == 0);
-        assert(scount(sem) == 32761);
-        assert(signaln(sem, 30000) == -2);
-        assert(scount(sem) == 32761);
-        assert(wait(sem) == 0);
-        assert(scount(sem) == 32760);
-        assert(signaln(sem, -2) < 0);
-        assert(scount(sem) == 32760);
-        assert(wait(sem) == 0);
-        assert(scount(sem) == 32759);
-        assert(signaln(sem, 8) == 0);
-        assert(scount(sem) == 32767);
-        assert(signaln(sem, 1) == -2);
-        assert(scount(sem) == 32767);
-        assert(signal(sem) == -2);
-        assert(scount(sem) == 32767);
-        for (int i=0; i<32767; i++) {
-            assert(wait(sem) == 0);
-        }
-        assert(try_wait(sem) == -3);
-        assert(scount(sem) == 0);
-        assert(sdelete(sem) == 0);
-        printf("ok.\n");
-/* -------------------------------------------------- */
-/* ---- END OF TEST 10 ------------------------------ */
-/* -------------------------------------------------- */
 
         init_process_stack();
         idle();
