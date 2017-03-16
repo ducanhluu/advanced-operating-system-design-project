f9b456403df457f3846893d1e25ad658edc28648
Duc-Anh Luu <Duc-Anh.Luu@grenoble-inp.org>
2017-03-16 07:09:58 +0100

    re-add console_putbytes.c

---
+++ user/lib/console_putbytes.c
@@ -0,0 +1,15 @@
+/*
+ * Ensimag - Projet système
+ * Copyright (C) 2012 - Damien Dejean <dam.dejean@gmail.com>
+ *
+ * Stub for console_putbytes system call.
+ */
+#include "sysapi.h"
+
+void console_putbytes(const char *s, int len) {
+        
+        (void)s;
+        (void)len;
+        /** To be implemented */
+}
+
